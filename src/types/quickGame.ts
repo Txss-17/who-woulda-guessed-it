49d2238266b4e57b01c7d8904d9c39d9b7f590a2
Txss-17 <gliyetat@gmail.com>
2025-08-14 08:38:22 +0200

    Update quickGame.ts

--- src/types/quickGame.ts
+++ src/types/quickGame.ts
@@ -1,1 +1,50 @@
 
+export type GameType = 'classic' | 'love' | 'friendly' | 'crazy' | 'party' | 'dirty';
+export type GameStatus = 'waiting' | 'playing';
+export type UserStatus = 'onroom' | 'offroom' | 'away';
+
+export interface Player {
+  id: string;
+  name: string;
+  avatar?: string;
+  status: UserStatus;
+}
+
+export interface OnroomGame {
+  id: string;
+  name: string;
+  players: {
+    count: number;
+    max: number;
+    list: Player[];
+  };
+  host: string;
+  type: GameType;
+  status: GameStatus;
+}
+
+export interface Message {
+  id: string;
+  senderId: string;
+  senderName: string;
+  recipientId?: string; // If undefined, it's a group message
+  gameId?: string; // If defined, it's a game message
+  content: string;
+  timestamp: Date;
+  read: boolean;
+}
+
+export const typeLabels: Record<GameType, string> = {
+  classic: 'Classique',
+  love: 'Amour',
+  friendly: 'Amitié',
+  crazy: 'Folie',
+  party: 'Fête',
+  dirty: 'Coquin'
+};
+
+export const userStatusLabels = {
+  onroom: 'Dans la salle',
+  offroom: 'Hors la salle',
+  away: 'Absent'
+};
